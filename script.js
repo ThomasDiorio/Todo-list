b2f34d8c4615030da18377d99ba538b409928f01
Thomas Diorio <trd9077@gmail.com>
2020-12-11 22:38:56 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let items = ['eggs', 'milk'];
+//let new =
+let input = prompt("What do you want to do?");
+
+while (input !== 'quit') {
+
+    if (input === 'new') {
+        input = prompt("Enter the item to add");
+        items.push(input);
+    }
+
+    else if (input === 'list') {
+        for (let i = 0; i < items.length; i++) {
+            console.log(items[i]);
+        }
+    }
+
+    else if (input === 'delete') {
+        input = prompt("Enter the index of item to delete");
+        items.splice(input, 1);
+
+    }
+
+    input = prompt("...What do you want to do?");
+}
+console.log("You quit");
